0c1192bccdbd06b40c9a60c33a2b8b0c76154e15
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-08-22 17:17:59 -0300

    solving type problems

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,6 +7,8 @@ export const globalStyles = globalCss({
     },
 
     body: {
+        backgroundColor: '$gray900',
+        color: '$gray100',
         'webkit-font-smoothing': 'antialiased',
     },
 
