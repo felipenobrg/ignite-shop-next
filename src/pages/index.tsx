f934fdee4761988f2466d06f546fcf830bfbcb7d
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-08-23 18:23:46 -0300

    creating cards of products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,29 @@
-import { styled } from "../styles"
+import { HomeContainer, Product } from "@/styles/pages/Home"
+import Image from "next/image"
 
-const Button = styled('button', {
-  backgroundColor: 'red',
-})
+import Camiseta1 from '../assets/camisetas/1.svg'
+import Camiseta2 from '../assets/camisetas/2.svg'
+import Camiseta3 from '../assets/camisetas/3.svg'
+
 
 export default function Home() {
   return (
-    <>
-      <Button>hi</Button>
-    </>
+    <HomeContainer>
+      <Product>
+        <Image src={Camiseta1} width={520} height={480}  alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={Camiseta2} width={520} height={480}  alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
